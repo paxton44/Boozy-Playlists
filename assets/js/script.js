ec771f4775750870600c01b3c8df244d5f2dd592
Mitchell Flood <mitchflood1@gmail.com>
2021-02-06 12:42:09 -0700

    adding drink js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,38 @@
 //Pseudo Code 
 
 //make function that grabs the API 
-//Var=APIGrabber = function (){}
+Var=APIGrabber = function (){}
 
 //API pseudo code rough: Spotify uses genres to access all media inside its database. 
-//get a list of all genres 
+
 //get the playlists
 //get the playlist items 
-//get the songs from the playlist individually 
+//get the songs from the playlist individually ****figure out if we need to go further into folders or if playlist is enough
 
 //Pseudo code code structure 
 //make a var that grabs from spotify 
+Var APIFunction =(function()) {
+var userID =  ""
+var userSecret = ""
+}
+//
+
 //it uses a token parameter from spotify 
+// 
+
+
+
 //it then fetches the spotify api
 //then grabs what you want based on how far into the database you went(genre, playlist, album, song)
 //then it returns the selected music in JSON for your app
 //then it returns to the app how the user expects to see the playlist or song 
 
 //
+
+
+//return function 
+// return {
+//     getPlaylist(token,genreID)}
+//     {
+// retrun playlist (token,genreID)
+//     }
